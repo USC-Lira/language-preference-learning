be9cb9f89533ae3c2a3ded73eaca1ed9c837ea42
Zhaojing Yang <victoryang1026@gmail.com>
2023-12-15 21:58:19 +0000

    limit cpu usage

--- feature_learning/learn_features.py
+++ feature_learning/learn_features.py
@@ -12,6 +12,7 @@ from feature_learning.model import NLTrajAutoencoder
 from feature_learning.utils import timeStamped, BERT_MODEL_NAME, BERT_OUTPUT_DIM, create_logger
 
 os.environ["TOKENIZERS_PARALLELISM"] = "false"
+os.environ["OMP_NUM_THREADS"] = "4"
 
 
 def train(logger, seed, data_dir, save_dir, epochs, batch_size, learning_rate=1e-3, weight_decay=0, encoder_hidden_dim=128,
@@ -183,7 +184,6 @@ def train(logger, seed, data_dir, save_dir, epochs, batch_size, learning_rate=1e
 
             # add the mini-batch training loss to epoch loss
             loss += train_loss.item()
-            break
 
         # compute the epoch training loss
         # Note: this is the per-BATCH loss. len(train_loader) gives number of batches.
@@ -222,7 +222,6 @@ def train(logger, seed, data_dir, save_dir, epochs, batch_size, learning_rate=1e
 
                 val_loss += (reconstruction_loss + log_likelihood_loss).item()
                 num_correct += np.sum(dot_prod.detach().cpu().numpy() > 0)
-                break
 
         val_loss /= len(val_loader)
         val_reconstruction_loss /= len(val_loader)
