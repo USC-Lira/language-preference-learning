1f91b4a489e48a22bbf5b61f6f2221554f039e11
zhaojing <victoryang1026@gmail.com>
2024-01-11 14:28:59 -0800

    add scripts for trajectory transformer

---
+++ scripts/finetune_bert_traj_transformer.sh
@@ -0,0 +1,6 @@
+# Script to learn features from the data. Co-train BERT and the trajectory encoder.
+for model in bert-tiny
+do
+  python -m feature_learning.learn_features --initial-loss-check --data-dir=data/dataset \
+  --exp-name=traj-trans-lang-finetune-$model-newdata --bert-model=$model --use-bert-encoder --batch-size=512 --epochs=2
+done
