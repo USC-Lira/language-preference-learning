c5336a271713441030b6f708c7eb889ffb63449d
Zhaojing Yang <victoryang1026@gmail.com>
2024-05-09 18:49:13 +0000

    add a script for resizing images

---
+++ scripts/resize.sh
@@ -0,0 +1,3 @@
+python data/transform.py --transform=resize --data-dir=data/data_avoid_danger/train
+python data/transform.py --transform=resize --data-dir=data/data_avoid_danger/val
+python data/transform.py --transform=resize --data-dir=data/data_avoid_danger/test
