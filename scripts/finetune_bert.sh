1c6a60a19b4c2c5955f69f768ea5a45556f441ef
lemonlemonde <mirudjun@gmail.com>
2024-05-27 01:55:13 +0000

    debug attempt, small changes

--- scripts/finetune_bert.sh
+++ scripts/finetune_bert.sh
@@ -1,7 +1,7 @@
-export CUDA_VISIBLE_DEVICES=5
+# export CUDA_VISIBLE_DEVICES=5
 # Script to learn features from the data. Co-train BERT and the trajectory encoder.
 for model in bert-tiny
 do
-  python -m lang_pref_learning.feature_learning.learn_features --initial-loss-check --data-dir=dataset \
+  python -m lang_pref_learning.feature_learning.learn_features --initial-loss-check --data-dir=/scr/zyang966/language-preference-learning/data/data_img_obs_res_224_more \
   --exp-name=finetune-$model-newdata --lang-model=$model --use-bert-encoder --batch-size=128 --epoch=1 --lr=5e-3
 done
